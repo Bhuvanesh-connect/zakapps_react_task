e1ba1b839c6356a9ee0fbd7caf08d03ab390d29f
DESKTOP-FIQETC7\Bhuvanesh <bhuvanesh.connect@gmail.com>
2026-01-29 03:24:56 +0530

    changing base-url for hosting

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/zakapps_react_task">
       <App />
     </BrowserRouter>
   </StrictMode>,
